5ccb9b61e0ee513bf002f8fe7bd6da36955fd563
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-09-06 11:38:15 -0500

    estilos a la vista de inicio

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -4,17 +4,15 @@ import img from '../images/fondo.jpg'
 function inicio(){
     const section = document.createElement('section')
     const image =document.createElement('img');
-        image.src =img;
-    const box =document.createElement('div')
-    box.setAttribute("id", "boxI");
-
+    image.src =img;
+    const title= document.createElement('h3');
+    const boxName =document.createElement('div')
+    boxName.setAttribute("id", "boxName");
     
     
+    title.textContent ='Valusic';
     
-    
-    
-    
-    section.append(image,box,);
+    section.append(image, title, boxName, );
     return section;
 }
 
