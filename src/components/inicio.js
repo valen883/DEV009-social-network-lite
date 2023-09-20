ea36902253f4ee53e8f475a1208b2d2f7e679a69
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-09-20 16:50:56 -0500

    funcionalidad eliminar post por medio de la publicación

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -2,65 +2,78 @@ import img from '../images/fondo.jpg'
 import { createPost, getLoggedInUser, getPosts, deletePost } from '../lib/services.js';
 
 
-function inicio(){
+function inicio() {
     const section = document.createElement('section');
-    const image =document.createElement('img');
+    const image = document.createElement('img');
 
-    const header =document.createElement('header');
-    const title= document.createElement('h3');
-    const boxName =document.createElement('div');
+    const header = document.createElement('header');
+    const title = document.createElement('h3');
+    const boxName = document.createElement('div');
 
-    const main= document.createElement('main');
-    const formPost= document.createElement('form');
+    const main = document.createElement('main');
+    const formPost = document.createElement('form');
     const inputPost = document.createElement('input');
-    const boxPublic =document.createElement('button');
+    const boxPublic = document.createElement('button');
     const divPost = document.createElement('div');
-    
-    
-    image.src =img;
-    inputPost.type ='text';
-    
+
+
+    image.src = img;
+    inputPost.type = 'text';
+
     boxName.setAttribute("id", "boxName");
     inputPost.setAttribute("id", "post");
     boxPublic.setAttribute("id", "botonPublic");
     divPost.setAttribute('id', 'dPost');
-    
+
 
     boxName.textContent = getLoggedInUser().email.slice(0, 10);
-    title.textContent ='Valusic';
-    inputPost.placeholder ='¿En qué estas pensando?';
+    title.textContent = 'Valusic';
+    inputPost.placeholder = '¿En qué estas pensando?';
     boxPublic.textContent = 'Publicar';
 
-    
-    function display(){
-        console.log(getPosts());
-        getPosts().forEach(post => {
-                    const divPublic = document.createElement('article');
-            
-                    divPublic.innerHTML  += ` <h4>${post.email.slice(0, 10)}</h4>
+
+    function display() {
+        divPost.innerHTML = '';
+          getPosts().forEach(post => {
+            const divPublic = document.createElement('article');
+            // console.log(post)
+            divPublic.innerHTML += ` <h4>${post.email.slice(0, 10)}</h4>
                      <p>${post.content}</p>
-                     <button class="delete-post" data-id="${post.id}">🗑</button>`
-                    divPost.append(divPublic);
+                     <button class="deletePost" value="${post.id}">🗑</button>
+                     <button>🖊</button>`
+
+            divPost.append(divPublic);
         });
-    }
+
+
+
+    const btnDelete = document.querySelectorAll('.deletePost');
+    // console.log(btnDelete)
+    btnDelete.forEach(btn => {
+        btn.addEventListener('click', function () {
+            console.log('funciona' + btn.value)
+            deletePost(btn.value);
+            display()
+         })
+        
+    })
+}
     display();
 
-    boxPublic.addEventListener('click', function(a){
+    boxPublic.addEventListener('click', function (a) {
         a.preventDefault();
         const texto = inputPost.value
-        console.log( texto);
+        console.log(texto);
         console.log(getLoggedInUser());
-        createPost(texto, getLoggedInUser().email )
+        createPost(texto, getLoggedInUser().email)
         display();
     })
-
-
 
     header.append(title, boxName);
     main.append(formPost, divPost);
     formPost.append(inputPost, boxPublic);
 
-    section.append(image,header, main);
+    section.append(image, header, main);
     return section;
 }
 
