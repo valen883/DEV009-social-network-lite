88dd04ed385488a523fd5e5cb4e749dd54c25300
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-10-09 17:10:26 -0500

    pruebas en jest simulación de data y funcion init

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,38 @@
 // importamos la funcion que vamos a testear
-import { init } from '../src/lib/services';
+import { init, register } from '../src/lib/index';
 
-describe('clearStorage', () => {
-  it('debería limpiar la información de localStorage', () => {
-    expect(typeof init).toBe('function');
+//simulacion de datos necesarios antes de correr las pruebas
+beforeEach(() => {
+  const users = [{ email: 'usuario@example.com', password: '123456' }];
+  const loggedInUser = { email: 'usuario@example.com', password: '123456' };
+  const posts = [
+    { id: 'posta', content: 'contenidoa', email: 'usuarioa@example.com' },
+    { id: 'postb', content: 'contenidob', email: 'usuariob@example.com' },
+  ];
+  global.localStorage = {
+    getItem: jest.fn((key) => {
+      switch (key) {
+        case 'users':
+          return JSON.stringify(users);
+        case 'user':
+          return JSON.stringify(loggedInUser);
+        case 'posts':
+          return JSON.stringify(posts);
+        default:
+          return null;
+      }
+    }),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  };
+});
+
+
+// test funcion init
+describe('init function', () => {
+  it('Should clear the localStorage', () => {
+    init();
+    expect(localStorage.clear).toHaveBeenCalled();
   });
 });
